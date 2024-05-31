32f73e2dc0ea683636a38cac33703d7f032759cd
sandeep.singh <sandeep.singh@postman.com>
2024-05-31 19:24:49 +0530

    base setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
